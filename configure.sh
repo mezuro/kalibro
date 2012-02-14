a0f057206e80a443b2751d649bf07364ed2172f9
Carlos Morais <carlos88morais@gmail.com>
2012-02-14 14:52:26 -0200

    Script for configure development environment

---
+++ configure.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git clone git@gitorious.org:kalibro/libraries.git
+git clone git@gitorious.org:kalibro/resources.git
+
+mv libraries Libraries
+mv resources Resources
+
+mkdir -pv $HOME/.kalibro
+rm -rf $HOME/.kalibro/tests
+tar -xvf Resources/tests.tar.gz -C $HOME/.kalibro
+
+echo "Should be done on Eclipse:"
+echo "1 - Install plugins Eclemma and Metrics"
+echo "2 - File->Import->Preferences for Resources/*.epf"
+echo "3 - Window->Preferences->Java->Build Path->User Libraries->Import with Libraries/Libraries.userlibraries"
+
+exit 0
