6945cb906f9661f5f5e97b222b66701165016dc3
Carlos Morais <carlos88morais@gmail.com>
2012-02-21 14:16:53 -0200

    Fix annotation style

--- KalibroCore/src/org/kalibro/core/model/abstracts/IdentityField.java
+++ KalibroCore/src/org/kalibro/core/model/abstracts/IdentityField.java
@@ -6,7 +6,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({FIELD})
+@Target(FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface IdentityField {
 	// Marks the field to be used by equals()
