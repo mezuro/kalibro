18a6a6c608bb690e4dab30e5ba506d51835730dd
Carlos Morais <carlos88morais@gmail.com>
2012-03-08 22:14:42 -0300

    Refactoring AddMetricDialogManualTest

--- KalibroTests/manual/org/kalibro/desktop/configuration/AddMetricDialogManualTest.java
+++ KalibroTests/manual/org/kalibro/desktop/configuration/AddMetricDialogManualTest.java
@@ -3,18 +3,22 @@ package org.kalibro.desktop.configuration;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class AddMetricDialogManualTest implements ActionListener {
-
-	private static AddMetricDialog dialog;
+public final class AddMetricDialogManualTest implements ActionListener {
 
 	public static void main(String[] args) {
+		new AddMetricDialogManualTest();
+	}
+
+	private AddMetricDialog dialog;
+
+	private AddMetricDialogManualTest() {
 		dialog = new AddMetricDialog();
-		dialog.addOkListener(new AddMetricDialogManualTest());
+		dialog.addOkListener(this);
 		dialog.setVisible(true);
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(ActionEvent event) {
 		dialog.dispose();
 		System.out.println("Metric: " + dialog.getMetric());
 	}
