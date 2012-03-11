34d133e8b50cbf848192446bd392509a30940334
Carlos Morais <carlos88morais@gmail.com>
2012-03-11 08:02:21 -0300

    Completing implementation of ConfigurationFrame

--- KalibroDesktop/src/org/kalibro/desktop/configuration/ConfigurationFrame.java
+++ KalibroDesktop/src/org/kalibro/desktop/configuration/ConfigurationFrame.java
@@ -1,5 +1,8 @@
 package org.kalibro.desktop.configuration;
 
+import java.awt.event.ContainerAdapter;
+import java.awt.event.ContainerEvent;
+
 import javax.swing.JInternalFrame;
 
 import org.kalibro.core.model.Configuration;
@@ -13,41 +16,47 @@ public class ConfigurationFrame extends JInternalFrame implements TablePanelList
 	private ConfigurationPanel configurationPanel;
 	private CardStackPanel cardStack;
 
+	private Configuration configuration;
+
 	public ConfigurationFrame(Configuration configuration) {
-		super(configuration.getName(), true, true, true, true);
+		super(configuration.getName() + " - Configuration", true, true, true, true);
 		new Icon(Icon.KALIBRO).replaceIconOf(this);
 		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-		buildContentPane(configuration);
 		setName("configuration");
-		setVisible(true);
 
+		this.configuration = configuration;
+		buildContentPane();
+		setVisible(true);
 	}
 
-	private void buildContentPane(Configuration configuration) {
+	private void buildContentPane() {
 		configurationPanel = new ConfigurationPanel();
 		configurationPanel.set(configuration);
 		configurationPanel.addMetricConfigurationsListener(this);
 		cardStack = new CardStackPanel();
 		cardStack.push(configurationPanel);
+		cardStack.addContainerListener(new CardStackListener());
 		setContentPane(cardStack);
 		pack();
 	}
 
 	@Override
 	public void add() {
-		Configuration configuration = configurationPanel.get();
-		createController(configuration).add();
-		configurationPanel.set(configuration);
+		configuration = configurationPanel.get();
+		new MetricConfigurationController(configuration, cardStack).add();
 	}
 
 	@Override
 	public void edit(MetricConfiguration metricConfiguration) {
-		Configuration configuration = configurationPanel.get();
-		createController(configuration).edit(metricConfiguration);
-		configurationPanel.set(configuration);
+		configuration = configurationPanel.get();
+		new MetricConfigurationController(configuration, cardStack).edit(metricConfiguration);
 	}
 
-	private MetricConfigurationController createController(Configuration configuration) {
-		return new MetricConfigurationController(configuration, cardStack);
+	private class CardStackListener extends ContainerAdapter {
+
+		@Override
+		public void componentRemoved(ContainerEvent event) {
+			configurationPanel.set(configuration);
+		}
 	}
 }
--- KalibroTests/unit/org/kalibro/desktop/configuration/ConfigurationFrameTest.java
+++ KalibroTests/unit/org/kalibro/desktop/configuration/ConfigurationFrameTest.java
@@ -3,6 +3,8 @@ package org.kalibro.desktop.configuration;
 import static org.junit.Assert.*;
 import static org.mockito.Matchers.*;
 
+import javax.swing.JPanel;
+import javax.swing.JTable;
 import javax.swing.WindowConstants;
 
 import org.junit.Before;
@@ -12,8 +14,11 @@ import org.kalibro.KalibroTestCase;
 import org.kalibro.core.model.Configuration;
 import org.kalibro.core.model.ConfigurationFixtures;
 import org.kalibro.core.model.MetricConfiguration;
-import org.mockito.InOrder;
+import org.kalibro.desktop.ComponentFinder;
+import org.kalibro.desktop.swingextension.Button;
+import org.kalibro.desktop.swingextension.panel.CardStackPanel;
 import org.mockito.Mockito;
+import org.mockito.internal.util.reflection.Whitebox;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareOnlyThisForTest;
@@ -24,11 +29,12 @@ import org.powermock.modules.junit4.PowerMockRunner;
 @PrepareOnlyThisForTest(ConfigurationFrame.class)
 public class ConfigurationFrameTest extends KalibroTestCase {
 
-	private MetricConfigurationController metricConfigurationController;
 	private Configuration configuration;
+	private ConfigurationPanel panel;
+	private MetricConfigurationController metricConfigurationController;
 
 	private ConfigurationFrame frame;
-	private ConfigurationPanel panel;
+	private ComponentFinder finder;
 
 	@Before
 	public void setUp() throws Exception {
@@ -36,23 +42,23 @@ public class ConfigurationFrameTest extends KalibroTestCase {
 		mockPanel();
 		mockMetricConfigurationController();
 		frame = new ConfigurationFrame(configuration);
+		finder = new ComponentFinder(frame);
 	}
 
 	private void mockPanel() throws Exception {
 		panel = PowerMockito.spy(new ConfigurationPanel());
-		PowerMockito.when(panel.get()).thenReturn(configuration);
 		PowerMockito.whenNew(ConfigurationPanel.class).withNoArguments().thenReturn(panel);
 	}
 
 	private void mockMetricConfigurationController() throws Exception {
 		metricConfigurationController = PowerMockito.mock(MetricConfigurationController.class);
-		PowerMockito.whenNew(MetricConfigurationController.class).withArguments(same(configuration), any())
+		PowerMockito.whenNew(MetricConfigurationController.class).withArguments(eq(configuration), any())
 			.thenReturn(metricConfigurationController);
 	}
 
 	@Test(timeout = UNIT_TIMEOUT)
-	public void titleShouldBeConfigurationName() {
-		assertEquals(configuration.getName(), frame.getTitle());
+	public void titleShouldHaveConfigurationName() {
+		assertEquals(configuration.getName() + " - Configuration", frame.getTitle());
 	}
 
 	@Test(timeout = UNIT_TIMEOUT)
@@ -92,20 +98,28 @@ public class ConfigurationFrameTest extends KalibroTestCase {
 
 	@Test(timeout = UNIT_TIMEOUT)
 	public void shouldAddMetricConfiguration() {
-		frame.add();
-		InOrder order = Mockito.inOrder(panel, metricConfigurationController, panel);
-		order.verify(panel).get();
-		order.verify(metricConfigurationController).add();
-		order.verify(panel).set(configuration);
+		finder.find("add", Button.class).doClick();
+		Mockito.verify(panel).get();
+		Mockito.verify(metricConfigurationController).add();
 	}
 
 	@Test(timeout = UNIT_TIMEOUT)
 	public void shouldEditMetricConfiguration() {
+		finder.find("metricConfigurations", JTable.class).getSelectionModel().setSelectionInterval(0, 0);
+		finder.find("edit", Button.class).doClick();
+
 		MetricConfiguration metricConfiguration = configuration.getMetricConfigurations().iterator().next();
-		frame.edit(metricConfiguration);
-		InOrder order = Mockito.inOrder(panel, metricConfigurationController, panel);
-		order.verify(panel).get();
-		order.verify(metricConfigurationController).edit(metricConfiguration);
-		order.verify(panel).set(configuration);
+		Mockito.verify(panel).get();
+		Mockito.verify(metricConfigurationController).edit(metricConfiguration);
+	}
+
+	@Test(timeout = UNIT_TIMEOUT)
+	public void shouldRefreshConfigurationWhenPanelIsRemovedFromCardStack() {
+		CardStackPanel cardStack = (CardStackPanel) Whitebox.getInternalState(frame, "cardStack");
+		cardStack.push(new JPanel());
+
+		Mockito.reset(panel);
+		cardStack.pop();
+		Mockito.verify(panel).set(configuration);
 	}
 }
