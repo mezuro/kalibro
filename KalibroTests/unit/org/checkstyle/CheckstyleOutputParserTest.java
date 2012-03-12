dd4db8c75c30c5620df8a5fb26fe057aee13ea67
Eduardo <companheiro.vermelho@gmail.com>
2012-03-12 15:47:03 -0300

    Update expected results for CheckstyleOutputParser. To avoid listing all metric results, we filter the result from CheckstyleStub

--- KalibroTests/unit/org/checkstyle/CheckstyleOutputParserTest.java
+++ KalibroTests/unit/org/checkstyle/CheckstyleOutputParserTest.java
@@ -1,41 +1,74 @@
 package org.checkstyle;
 
+import static org.checkstyle.CheckstyleMetric.*;
+
 import com.puppycrawl.tools.checkstyle.api.AuditEvent;
 import com.puppycrawl.tools.checkstyle.api.LocalizedMessage;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.kalibro.KalibroTestCase;
+import org.kalibro.core.model.NativeMetric;
+import org.kalibro.core.model.NativeModuleResult;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({AuditEvent.class, LocalizedMessage.class})
 public class CheckstyleOutputParserTest extends KalibroTestCase {
 
 	private CheckstyleOutputParser parser;
+	private HashSet<NativeMetric> wantedMetricsSet;
 
 	@Before
 	public void setUp() {
-		parser = new CheckstyleOutputParser(CheckstyleStub.nativeMetrics());
+		NativeMetric[] wantedMetrics = {
+			AVERAGE_ANONYMOUS_CLASSES_LENGTH.getNativeMetric(),
+			AVERAGE_METHOD_LENGTH.getNativeMetric(),
+			EXECUTABLE_STATEMENTS.getNativeMetric(),
+			FILE_LENGTH.getNativeMetric(),
+			NUMBER_OF_METHODS.getNativeMetric(),
+			NUMBER_OF_OUTER_TYPES.getNativeMetric(),
+		};
+
+		wantedMetricsSet = new HashSet<NativeMetric>(Arrays.asList(wantedMetrics));
+		parser = new CheckstyleOutputParser(wantedMetricsSet);
 	}
 
 	@Test(timeout = UNIT_TIMEOUT)
 	public void shouldParseMetricResults() {
 		parser.auditStarted(null);
 		simulateCheckstyle();
-		assertDeepEquals(CheckstyleStub.results(), parser.getResults());
+		
+		Set<NativeModuleResult> expected = filterStubResultWithWantedMetrics();
+		assertDeepEquals(expected, parser.getResults());
+	}
+
+	private Set<NativeModuleResult> filterStubResultWithWantedMetrics() {
+		NativeModuleResult stubResult = CheckstyleStub.result();
+		NativeModuleResult expectedModuleResult = new NativeModuleResult(stubResult.getModule());
+		
+		for (NativeMetric wantedMetric: wantedMetricsSet)
+			if (stubResult.hasResultFor(wantedMetric))
+				expectedModuleResult.addMetricResult(stubResult.getResultFor(wantedMetric));
+		
+		return new HashSet<NativeModuleResult>(Arrays.asList(expectedModuleResult));
 	}
 
 	public void simulateCheckstyle() {
-		parser.addError(mockEvent("maxLen.anonInner", "0"));
-		parser.addError(mockEvent("maxLen.method", "3"));
-		parser.addError(mockEvent("executableStatementCount", "1"));
-		parser.addError(mockEvent("maxLen.file", "6"));
-		parser.addError(mockEvent("too.many.methods", "1"));
-		parser.addError(mockEvent("maxOuterTypes", "1"));
+		parser.addError(mockEvent("maxLen.anonInner", "6"));
+		parser.addError(mockEvent("maxLen.method", "" + 56.0 / 7.0));
+		parser.addError(mockEvent("executableStatementCount", "19"));
+		parser.addError(mockEvent("maxLen.file", "58"));
+		parser.addError(mockEvent("too.many.methods", "6"));
+		parser.addError(mockEvent("maxOuterTypes", "2"));
 		parser.auditFinished(null);
 	}
 
@@ -45,7 +78,7 @@ public class CheckstyleOutputParserTest extends KalibroTestCase {
 
 		AuditEvent event = PowerMockito.mock(AuditEvent.class);
 		PowerMockito.when(event.getLocalizedMessage()).thenReturn(localizedMessage);
-		PowerMockito.when(event.getFileName()).thenReturn("HelloWorld.java");
+		PowerMockito.when(event.getFileName()).thenReturn("Fibonacci.java");
 		PowerMockito.when(event.getMessage()).thenReturn(message);
 		return event;
 	}
