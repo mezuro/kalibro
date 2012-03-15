2ba2a0d15fb9c738a4f3f62cb9298f179d83357c
Carlos Morais <carlos88morais@gmail.com>
2012-03-14 21:34:01 -0300

    Integration test fix

--- KalibroTests/integration/org/kalibro/core/persistence/CsvExportTest.java
+++ KalibroTests/integration/org/kalibro/core/persistence/CsvExportTest.java
@@ -1,7 +1,6 @@
 package org.kalibro.core.persistence;
 
 import static org.junit.Assert.*;
-import static org.kalibro.core.model.ModuleResultFixtures.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,14 +12,21 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.kalibro.KalibroTestCase;
+import org.kalibro.core.model.ConfigurationFixtures;
+import org.kalibro.core.model.ModuleResult;
+import org.kalibro.core.model.ModuleResultFixtures;
 
 public class CsvExportTest extends KalibroTestCase {
 
 	private static final File CSV_FILE = new File(TESTS_DIRECTORY, "HelloWorld.csv");
 
+	private ModuleResult moduleResult;
+
 	@Before
 	public void setUp() {
 		assertFalse(CSV_FILE.exists());
+		moduleResult = ModuleResultFixtures.helloWorldClassResult();
+		moduleResult.setConfiguration(ConfigurationFixtures.simpleConfiguration());
 	}
 
 	@After
@@ -31,7 +37,7 @@ public class CsvExportTest extends KalibroTestCase {
 	@Test(timeout = INTEGRATION_TIMEOUT)
 	public void testExport() throws IOException {
 		InputStream expectedCsv = getClass().getResourceAsStream("HelloWorld.csv");
-		new ModuleResultCsvExporter(helloWorldClassResult()).exportTo(CSV_FILE);
+		new ModuleResultCsvExporter(moduleResult).exportTo(CSV_FILE);
 		assertEquals(IOUtils.toString(expectedCsv), FileUtils.readFileToString(CSV_FILE));
 	}
 }
