26d502c342849ae4244ee2c148c70cda0a3081c3
Carlos Morais <carlos88morais@gmail.com>
2012-03-14 22:39:38 -0300

    Refactoring acceptance tests

--- KalibroTests/acceptance/org/kalibro/desktop/settings/FirstEditSettings.java
+++ KalibroTests/acceptance/org/kalibro/desktop/settings/FirstEditSettings.java
@@ -15,37 +15,19 @@ import org.kalibro.core.settings.KalibroSettings;
  */
 public class FirstEditSettings extends KalibroDesktopTestCase {
 
-	private KalibroSettings settings;
-
 	@Test(timeout = ACCEPTANCE_TIMEOUT)
 	public void firstEditSettings() throws Exception {
 		startFromMain();
-		editSettings();
+
+		fixture.checkBox("removeSources").requireSelected();
+		fixture.checkBox("removeSources").uncheck();
 
 		fixture.button("ok").click();
 		assertTrue(KalibroSettings.settingsFileExists());
-		assertDeepEquals(settings, KalibroSettings.load());
 
 		fixture = new FrameFixture(fixture.robot, "kalibroFrame");
-		verifyDatabaseSettings();
-	}
-
-	private void editSettings() {
-		settings = new KalibroSettings();
-		settings.getDatabaseSettings().setJdbcUrl("First");
-		settings.getDatabaseSettings().setUsername("Edit");
-		settings.getDatabaseSettings().setPassword("Settings");
-
-		fixture.textBox("jdbcUrl").setText(settings.getDatabaseSettings().getJdbcUrl());
-		fixture.textBox("username").setText(settings.getDatabaseSettings().getUsername());
-		fixture.textBox("password").setText(settings.getDatabaseSettings().getPassword());
-	}
-
-	private void verifyDatabaseSettings() {
 		fixture.menuItem("settings").click();
-		fixture.dialog().textBox("jdbcUrl").requireText(settings.getDatabaseSettings().getJdbcUrl());
-		fixture.dialog().textBox("username").requireText(settings.getDatabaseSettings().getUsername());
-		fixture.dialog().textBox("password").requireText(settings.getDatabaseSettings().getPassword());
-		fixture.dialog().button("cancel").click();
+		fixture = fixture.dialog();
+		fixture.checkBox("removeSources").requireNotSelected();
 	}
 }
