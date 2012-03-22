979ba3621864d8ac0aa54c601aa614bacaa4aad8
Carlos Morais + Eduardo Morais <carlos88morais@gmail.com>
2012-03-22 15:21:41 -0300

    Implements client for MetricConfigurationEndpoint

---
+++ KalibroCore/src/org/kalibro/client/dao/MetricConfigurationPortDao.java
@@ -0,0 +1,31 @@
+package org.kalibro.client.dao;
+
+import org.kalibro.client.EndpointPortFactory;
+import org.kalibro.core.model.MetricConfiguration;
+import org.kalibro.core.persistence.dao.MetricConfigurationDao;
+import org.kalibro.service.MetricConfigurationEndpoint;
+import org.kalibro.service.entities.MetricConfigurationXml;
+
+public class MetricConfigurationPortDao implements MetricConfigurationDao {
+
+	private MetricConfigurationEndpoint port;
+
+	protected MetricConfigurationPortDao() {
+		port = EndpointPortFactory.getEndpointPort(MetricConfigurationEndpoint.class);
+	}
+
+	@Override
+	public void save(MetricConfiguration metricConfiguration, String configurationName) {
+		port.saveMetricConfiguration(new MetricConfigurationXml(metricConfiguration), configurationName);
+	}
+
+	@Override
+	public MetricConfiguration getMetricConfiguration(String configurationName, String metricName) {
+		return port.getMetricConfiguration(configurationName, metricName).convert();
+	}
+
+	@Override
+	public void removeMetricConfiguration(String configurationName, String metricName) {
+		port.removeMetricConfiguration(configurationName, metricName);
+	}
+}
