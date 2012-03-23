c22829a430228e6a43ed3c1c9812308a0eb2761c
Carlos Morais <carlos88morais@gmail.com>
2012-03-23 13:00:23 -0300

    Explaining project process improvement

--- KalibroCore/src/org/kalibro/core/processing/ProcessProjectTask.java
+++ KalibroCore/src/org/kalibro/core/processing/ProcessProjectTask.java
@@ -4,6 +4,11 @@ import org.kalibro.Kalibro;
 import org.kalibro.core.concurrent.Task;
 import org.kalibro.core.model.Project;
 
+/*
+ * TODO Make possible to add listener to be notified when whole process finishes. This is desirable for
+ * integration-testing the whole process. Not possible yet because each sub-task is run in different threads with
+ * different listeners, and this task just starts the others.
+ */
 public class ProcessProjectTask extends Task {
 
 	private Project project;
@@ -14,7 +19,6 @@ public class ProcessProjectTask extends Task {
 
 	@Override
 	public void perform() {
-		// TODO execute all parts of project processing in the same thread
 		new LoadProjectExecutor(project).execute();
 	}
 }
