970e5f68ddfe9a1a0559bc92e19b9ac30f3bd4c2
Carlos Morais <carlos88morais@gmail.com>
2012-04-17 11:12:22 -0300

    Adding assertMatches() to KalibroTestCase
    And using it on StackTracePrinterTest

--- KalibroTests/unit/org/kalibro/core/util/StackTracePrinterTest.java
+++ KalibroTests/unit/org/kalibro/core/util/StackTracePrinterTest.java
@@ -13,17 +13,17 @@ public class StackTracePrinterTest extends KalibroTestCase {
 
 	private static final Exception EXCEPTION = new Exception("StackTracePrinterTest message");
 
-	private StackTracePrinter printer;
+	private StackTracePrinter printer = new StackTracePrinter();
 
 	@Test(timeout = UNIT_TIMEOUT)
 	public void shouldPrintStackTraceWithoutMaximum() throws IOException {
-		printer = new StackTracePrinter();
-		assertTrue(expected().matcher(printer.printStackTrace(EXCEPTION)).matches());
+		String regularExpression = IOUtils.toString(StackTracePrinterTest.class.getResourceAsStream("stackTrace.txt"));
+		Pattern expected = Pattern.compile(regularExpression, Pattern.MULTILINE);
+		assertMatches(expected, printer.printStackTrace(EXCEPTION));
 	}
 
 	@Test(timeout = UNIT_TIMEOUT)
 	public void shouldResetPrintStackTrace() {
-		printer = new StackTracePrinter();
 		String first = printer.printStackTrace(EXCEPTION);
 		String second = printer.printStackTrace(EXCEPTION);
 		assertEquals(first, second);
@@ -33,10 +33,5 @@ public class StackTracePrinterTest extends KalibroTestCase {
 	public void shouldRestrictBytesOfStackTraceWithMaximum() {
 		printer = new StackTracePrinter(42);
 		assertEquals("java.lang.Exception: StackTracePrinterTest", printer.printStackTrace(EXCEPTION));
-	}
-
-	private Pattern expected() throws IOException {
-		String regexp = IOUtils.toString(StackTracePrinterTest.class.getResourceAsStream("stackTrace.txt"));
-		return Pattern.compile(regexp, Pattern.MULTILINE);
 	}
 }
