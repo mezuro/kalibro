05a7a2062ffe0784894895ec7888f2a9a3753310
Carlos Morais <carlos88morais@gmail.com>
2012-05-11 14:58:59 -0300

    Abstract InternalFrame

---
+++ KalibroDesktop/src/org/kalibro/desktop/swingextension/InternalFrame.java
@@ -0,0 +1,42 @@
+package org.kalibro.desktop.swingextension;
+
+import java.awt.Container;
+import java.beans.PropertyVetoException;
+
+import javax.swing.JInternalFrame;
+
+import org.kalibro.KalibroException;
+import org.kalibro.desktop.swingextension.icon.Icon;
+
+public abstract class InternalFrame<T> extends JInternalFrame {
+
+	protected T entity;
+
+	public InternalFrame(String name, String title, T entity) {
+		super(title, true, true, true, true);
+		new Icon(Icon.KALIBRO).replaceIconOf(this);
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		setName(name);
+
+		this.entity = entity;
+		setContentPane(buildContentPane());
+		adjustSize();
+		setVisible(true);
+	}
+
+	protected abstract Container buildContentPane();
+
+	private void adjustSize() {
+		pack();
+		setMinimumSize(getPreferredSize());
+		setSize(getPreferredSize());
+	}
+
+	public void select() {
+		try {
+			setSelected(true);
+		} catch (PropertyVetoException exception) {
+			throw new KalibroException("Could not select " + getName() + " frame: " + entity, exception);
+		}
+	}
+}
