157b93fc20755808ba50a396028798daac18a758
Carlos Morais <carlos88morais@gmail.com>
2012-05-13 12:09:52 -0300

    ConfigurationMenu extends CrudMenu

--- KalibroDesktop/src/org/kalibro/desktop/configuration/ConfigurationMenu.java
+++ KalibroDesktop/src/org/kalibro/desktop/configuration/ConfigurationMenu.java
@@ -1,52 +1,24 @@
 package org.kalibro.desktop.configuration;
 
-import java.awt.Component;
-
 import javax.swing.JDesktopPane;
-import javax.swing.event.MenuEvent;
+import javax.swing.JInternalFrame;
 
-import org.kalibro.desktop.swingextension.menu.AbstractMenu;
-import org.kalibro.desktop.swingextension.menu.ReflectionMenuItem;
+import org.kalibro.core.model.Configuration;
+import org.kalibro.desktop.CrudMenu;
 
-public class ConfigurationMenu extends AbstractMenu {
-
-	private JDesktopPane desktopPane;
-	private ConfigurationController controller;
-
-	private ReflectionMenuItem newItem, openItem, deleteItem, saveItem, saveAsItem, closeItem;
+public class ConfigurationMenu extends CrudMenu<Configuration> {
 
 	public ConfigurationMenu(JDesktopPane desktopPane) {
-		super("configuration", "Configuration", 'C', desktopPane);
+		super(desktopPane, "Configuration");
 	}
 
 	@Override
-	protected void createItems(Component... innerComponents) {
-		desktopPane = (JDesktopPane) innerComponents[0];
+	protected void initializeController() {
 		controller = new ConfigurationController(desktopPane);
-		newItem = new ReflectionMenuItem("new", "New", 'N', controller, "newEntity");
-		openItem = new ReflectionMenuItem("open", "Open", 'O', controller, "open");
-		deleteItem = new ReflectionMenuItem("delete", "Delete", 'D', controller, "delete");
-		saveItem = new ReflectionMenuItem("save", "Save", 'S', controller, "save");
-		saveAsItem = new ReflectionMenuItem("saveAs", "Save as...", 'a', controller, "saveAs");
-		closeItem = new ReflectionMenuItem("close", "Close", 'l', controller, "close");
 	}
 
 	@Override
-	protected void buildMenu() {
-		add(newItem);
-		add(openItem);
-		add(deleteItem);
-		addSeparator();
-		add(saveItem);
-		add(saveAsItem);
-		add(closeItem);
-	}
-
-	@Override
-	public void menuSelected(MenuEvent event) {
-		boolean hasSelectedConfiguration = desktopPane.getSelectedFrame() instanceof ConfigurationFrame;
-		saveItem.setEnabled(hasSelectedConfiguration);
-		saveAsItem.setEnabled(hasSelectedConfiguration);
-		closeItem.setEnabled(hasSelectedConfiguration);
+	protected boolean isEntityFrame(JInternalFrame frame) {
+		return frame instanceof ConfigurationFrame;
 	}
 }
