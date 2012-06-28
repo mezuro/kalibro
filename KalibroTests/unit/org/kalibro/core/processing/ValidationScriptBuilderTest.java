b83922d3222aa09a88634eae4fe014ed657433d7
Carlos Morais <carlos88morais@gmail.com>
2012-06-28 16:53:33 -0300

    Validating native metric codes

--- KalibroTests/unit/org/kalibro/core/processing/ValidationScriptBuilderTest.java
+++ KalibroTests/unit/org/kalibro/core/processing/ValidationScriptBuilderTest.java
@@ -2,6 +2,7 @@ package org.kalibro.core.processing;
 
 import static org.junit.Assert.*;
 import static org.kalibro.core.model.ConfigurationFixtures.*;
+import static org.kalibro.core.model.MetricConfigurationFixtures.*;
 import static org.kalibro.core.model.MetricFixtures.*;
 
 import org.junit.Before;
@@ -24,9 +25,15 @@ public class ValidationScriptBuilderTest extends KalibroTestCase {
 	}
 
 	@Test(timeout = UNIT_TIMEOUT)
-	public void testScript() {
+	public void testCompoundScript() {
 		ValidationScriptBuilder scriptBuilder = new ValidationScriptBuilder(configuration, scConfiguration);
 		String expected = "var cbo = 1.0;\nvar lcom4 = 1.0;\nfunction sc(){return cbo * lcom4;}\n";
 		assertEquals(expected, scriptBuilder.buildScript());
+	}
+
+	@Test(timeout = UNIT_TIMEOUT)
+	public void testNativeScript() {
+		ValidationScriptBuilder scriptBuilder = new ValidationScriptBuilder(configuration, metricConfiguration("cbo"));
+		assertEquals("function cbo(){return 1.0;}", scriptBuilder.buildScript());
 	}
 }
