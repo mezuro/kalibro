062179b3cf4d0d11e8e19dde8469fbad6a22df54
Carlos Morais <carlos88morais@gmail.com>
2012-08-09 15:10:08 -0300

    CVSAnalyMetricTest extends EnumerationTestCase

--- KalibroTests/unit/org/cvsanaly/CVSAnalyMetricTest.java
+++ KalibroTests/unit/org/cvsanaly/CVSAnalyMetricTest.java
@@ -1,14 +1,11 @@
 package org.cvsanaly;
 
-import static org.cvsanaly.CVSAnalyMetric.*;
-import static org.junit.Assert.*;
 import static org.kalibro.core.model.enums.Granularity.*;
 
 import org.cvsanaly.entities.MetricResult;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.kalibro.KalibroTestCase;
+import org.kalibro.EnumerationTestCase;
 import org.kalibro.core.model.NativeMetric;
 import org.kalibro.core.model.enums.Language;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -16,20 +13,13 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(CVSAnalyMetric.class)
-public class CVSAnalyMetricTest extends KalibroTestCase {
-	
-	@BeforeClass
-	public static void emmaCoverage() {
-		CVSAnalyMetric.values();
-		CVSAnalyMetric.valueOf("NUMBER_OF_LINES_OF_CODE");
+public class CVSAnalyMetricTest extends EnumerationTestCase<CVSAnalyMetric> {
+
+	@Override
+	protected Class<CVSAnalyMetric> enumerationClass() {
+		return CVSAnalyMetric.class;
 	}
-	
-	@Test(timeout = UNIT_TIMEOUT)
-	public void checkToString() {
-		assertEquals("Number of lines of code", "" + NUMBER_OF_LINES_OF_CODE);
-		assertEquals("Number of comments", "" + NUMBER_OF_COMMENTS);
-	}
-	
+
 	@Test(timeout = UNIT_TIMEOUT)
 	public void checkNativeMetrics() {
 		for (CVSAnalyMetric metric : CVSAnalyMetric.values()) {
@@ -38,15 +28,14 @@ public class CVSAnalyMetricTest extends KalibroTestCase {
 			assertDeepEquals(expected, metric.getNativeMetric());
 		}
 	}
-	
+
 	@Test(timeout = UNIT_TIMEOUT)
 	public void shouldRetrieveCorrectField() {
 		MetricResult exampleResult = CVSAnalyStub.getExampleEntities().get(1);
 		double actual = CVSAnalyMetric.NUMBER_OF_SOURCE_LINES_OF_CODE.getMetricValue(exampleResult);
 		assertDoubleEquals(exampleResult.getNumberOfSourceLinesOfCode(), actual);
-		
+
 		actual = CVSAnalyMetric.NUMBER_OF_COMMENTS.getMetricValue(exampleResult);
 		assertDoubleEquals(exampleResult.getNumberOfComments(), actual);
 	}
-	
 }
