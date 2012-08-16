a4949d16f80cae2b6c1ce0be58c5d8fb2c45f5bc
Carlos Morais <carlos88morais@gmail.com>
2012-08-15 21:08:54 -0300

    KalibroError constructor with just message

--- KalibroCore/src/org/kalibro/KalibroError.java
+++ KalibroCore/src/org/kalibro/KalibroError.java
@@ -4,6 +4,10 @@ public class KalibroError extends KalibroException {
 
 	public static final String MESSAGE_PREFIX = "Please report this bug: ";
 
+	public KalibroError(String message) {
+		super(MESSAGE_PREFIX + message);
+	}
+
 	public KalibroError(String message, Throwable cause) {
 		super(MESSAGE_PREFIX + message, cause);
 	}
--- KalibroTests/unit/org/kalibro/KalibroErrorTest.java
+++ KalibroTests/unit/org/kalibro/KalibroErrorTest.java
@@ -2,31 +2,20 @@ package org.kalibro;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.kalibro.core.util.StackTracePrinter;
 
 public class KalibroErrorTest extends KalibroTestCase {
 
 	private static final String MESSAGE = "KalibroErrorTest message";
 
-	private Throwable cause;
-	private KalibroError error;
-
-	@Before
-	public void setUp() {
-		cause = new Exception();
-		error = new KalibroError(MESSAGE, cause);
+	@Test(timeout = UNIT_TIMEOUT)
+	public void shouldAddPrefixBeforeMessage() {
+		assertEquals("Please report this bug: " + MESSAGE, new KalibroError(MESSAGE).getMessage());
 	}
 
 	@Test(timeout = UNIT_TIMEOUT)
-	public void checkContructor() {
-		assertEquals("Please report this bug: " + MESSAGE, error.getMessage());
-		assertSame(cause, error.getCause());
-	}
-
-	@Test(timeout = UNIT_TIMEOUT)
-	public void shouldPrintStackTraceToString() {
-		assertEquals(new StackTracePrinter().printStackTrace(error), error.getPrintedStackTrace());
+	public void shouldAllowConstructionWithCause() {
+		Throwable cause = new Exception();
+		assertSame(cause, new KalibroError(MESSAGE, cause).getCause());
 	}
 }
