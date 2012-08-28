3aa01c17dc4e4618708ff054e0bf4c137632030e
Alessandro Palmeira + Eduardo Morais <alessandro.palmeira@gmail.com>
2012-08-28 13:21:05 -0300

    Added unit tests to cover entities

---
+++ KalibroTests/src/org/cvsanaly/DataObjectTest.java
@@ -0,0 +1,48 @@
+package org.cvsanaly;
+
+import static org.junit.Assert.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import org.junit.Test;
+import org.kalibro.TestCase;
+import org.kalibro.core.util.Identifier;
+import org.mockito.Mockito;
+
+public abstract class DataObjectTest<T extends Object> extends TestCase {
+
+	@Test(timeout = UNIT_TIMEOUT)
+	public void testDataObject() throws Exception {
+		for (Field field : getTypeClass().getDeclaredFields()) {
+			if (field.getAnnotations().length == 0)
+				continue;
+			Method getter, setter;
+			T testObject = getTypeClass().newInstance();
+			String fieldName = Identifier.fromVariable(field.getName()).asClassName();
+			getter = getTypeClass().getDeclaredMethod("get" + fieldName);
+			setter = getTypeClass().getDeclaredMethod("set" + fieldName, field.getType());
+			
+			Object testValue = getTestValue(field.getType());
+			setter.invoke(testObject, testValue);
+			assertEquals(testValue, getter.invoke(testObject));
+		}
+	}
+
+	private Object getTestValue(Class<?> fieldType) {
+		Object toReturn;
+		if (fieldType.equals(Long.TYPE))
+			toReturn = new Long(42);
+		else if (fieldType.equals(Double.TYPE))
+			toReturn = new Double(42);
+		else if (fieldType.equals(Integer.TYPE))
+			toReturn = new Integer(42);
+		else if (fieldType.equals(String.class))
+			toReturn = "42";
+		else 
+		toReturn = Mockito.mock(fieldType);
+		return toReturn;
+	}
+
+	protected abstract Class<T> getTypeClass();
+
+}
---
+++ KalibroTests/unit/org/cvsanaly/entities/MetricResultTest.java
@@ -0,0 +1,13 @@
+package org.cvsanaly.entities;
+
+import org.cvsanaly.DataObjectTest;
+
+
+public class MetricResultTest extends DataObjectTest<MetricResult> {
+
+	@Override
+	protected Class<MetricResult> getTypeClass() {
+		return MetricResult.class;
+	}
+
+}
