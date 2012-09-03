c640b9b732f107c4934d295abdad23b7b4d9fa0f
Carlos Morais <carlos88morais@gmail.com>
2012-09-03 12:59:35 -0300

    CVSAnalyTest as acceptance test

--- KalibroTests/acceptance/org/cvsanaly/CVSAnalyTest.java
+++ KalibroTests/acceptance/org/cvsanaly/CVSAnalyTest.java
@@ -1,13 +1,9 @@
 package org.cvsanaly;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintStream;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.kalibro.TestCase;
@@ -16,32 +12,20 @@ import org.kalibro.core.model.NativeMetric;
 public class CVSAnalyTest extends TestCase {
 
 	private CVSAnalyMetricCollector cvsanaly;
-	private PrintStream originalSysout;
 
 	@Before
 	public void setUp() {
-		originalSysout = System.out;
-		System.setOut(new PrintStream(new OutputStream() {
-
-			@Override
-			public void write(int arg0) throws IOException { /* Do nothing. This is to silence the console. */}
-		}));
 		cvsanaly = new CVSAnalyMetricCollector();
 	}
 
-	@After
-	public void tearDown() {
-		System.setOut(originalSysout);
-	}
-
-	@Test(timeout = 2 * INTEGRATION_TIMEOUT /* TODO optimize */)
+	@Test(timeout = ACCEPTANCE_TIMEOUT)
 	public void shouldCollectMetrics() throws Exception {
 		File codeDirectory = new File(samplesDirectory(), "cvsanaly");
 		Set<NativeMetric> metrics = cvsanaly.getBaseTool().getSupportedMetrics();
 		assertDeepEquals(CVSAnalyStub.results(), cvsanaly.collectMetrics(codeDirectory, metrics));
 	}
 
-	@Test(timeout = 2 * INTEGRATION_TIMEOUT /* TODO optimize */)
+	@Test(timeout = ACCEPTANCE_TIMEOUT)
 	public void shouldCollectSomeMetrics() throws Exception {
 		File codeDirectory = new File(samplesDirectory(), "cvsanaly");
 		Set<NativeMetric> metrics = new HashSet<NativeMetric>();
