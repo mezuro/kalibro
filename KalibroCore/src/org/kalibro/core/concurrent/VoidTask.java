b796edf2df87810a3ba250eb3167107a45cb4fe6
Carlos Morais <carlos88morais@gmail.com>
2012-09-20 00:33:28 -0300

    Removed unnecessary class ThreadInterrupter

--- KalibroCore/src/org/kalibro/core/concurrent/VoidTask.java
+++ KalibroCore/src/org/kalibro/core/concurrent/VoidTask.java
@@ -1,5 +1,11 @@
 package org.kalibro.core.concurrent;
 
+/**
+ * This class exists as convenience for creating void tasks (those which do not produce result) without the ugliness of
+ * a {@link Void} return type and a {@code return null;} statement.
+ * 
+ * @author Carlos Morais
+ */
 public abstract class VoidTask extends Task<Void> {
 
 	@Override
