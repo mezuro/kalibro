f73969395fb17b237d51104ffbe83b298ad41738
Carlos Morais <carlos88morais@gmail.com>
2012-09-23 10:34:27 -0300

    Complete merge

--- KalibroTests/unit/org/kalibro/core/settings/MailSettingsTest.java
+++ KalibroTests/unit/org/kalibro/core/settings/MailSettingsTest.java
@@ -7,13 +7,13 @@ import org.codemonkey.simplejavamail.TransportStrategy;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.kalibro.MailSettings;
-import org.kalibro.TestCase;
+import org.kalibro.tests.UnitTest;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(MailSettings.class)
-public class MailSettingsTest extends TestCase {
+public class MailSettingsTest extends UnitTest {
 
 	private MailSettings settings = new MailSettings();
 
