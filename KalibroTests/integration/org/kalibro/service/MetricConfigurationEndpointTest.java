1d72131d0d61f6266a8e2335b04dbe662aa49e6a
Carlos Morais <carlos88morais@gmail.com>
2012-09-23 11:23:41 -0300

    Update MetricConfigurationEndointTest

--- KalibroTests/integration/org/kalibro/service/MetricConfigurationEndpointTest.java
+++ KalibroTests/integration/org/kalibro/service/MetricConfigurationEndpointTest.java
@@ -1,61 +1,37 @@
 package org.kalibro.service;
 
-import static org.junit.Assert.*;
 import static org.kalibro.core.model.MetricConfigurationFixtures.metricConfiguration;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.kalibro.core.model.MetricConfiguration;
 import org.kalibro.dao.MetricConfigurationDao;
 import org.kalibro.service.entities.MetricConfigurationXml;
 
-public class MetricConfigurationEndpointTest extends OldEndpointTest {
+public class MetricConfigurationEndpointTest extends
+	EndpointTest<MetricConfiguration, MetricConfigurationDao, MetricConfigurationEndpoint> {
 
-	private static final String CONFIGURATION_NAME = "MetricConfigurationEndpointTest";
+	private static final String CONFIGURATION_NAME = "MetricConfigurationEndpointTest configuration name";
 
-	private MetricConfigurationDao daoFake;
-	private MetricConfigurationEndpoint port;
-
-	@Before
-	public void setUp() {
-		daoFake = new MetricConfigurationDaoFake();
-		daoFake.save(metricConfiguration("cbo"), CONFIGURATION_NAME);
-		daoFake.save(metricConfiguration("loc"), CONFIGURATION_NAME);
-		port = publishAndGetPort(new MetricConfigurationEndpointImpl(daoFake), MetricConfigurationEndpoint.class);
+	@Override
+	protected MetricConfiguration loadFixture() {
+		return metricConfiguration("cbo");
 	}
 
 	@Test
 	public void shouldGetMetricConfigurationByName() {
-		verifyGetMetricConfiguration("cbo");
-		verifyGetMetricConfiguration("loc");
-	}
-
-	private void verifyGetMetricConfiguration(String code) {
-		String metricName = metricConfiguration(code).getMetric().getName();
-		MetricConfiguration expected = daoFake.getMetricConfiguration(CONFIGURATION_NAME, metricName);
-		MetricConfiguration actual = port.getMetricConfiguration(CONFIGURATION_NAME, metricName).convert();
-		assertDeepEquals(expected, actual);
+		when(dao.getMetricConfiguration(CONFIGURATION_NAME, "42")).thenReturn(entity);
+		assertDeepDtoEquals(entity, port.getMetricConfiguration(CONFIGURATION_NAME, "42"));
 	}
 
 	@Test
 	public void shouldRemoveConfigurationByName() {
-		verifyRemoveMetricConfiguration("cbo");
-		verifyRemoveMetricConfiguration("loc");
-	}
-
-	private void verifyRemoveMetricConfiguration(String code) {
-		String metricName = metricConfiguration(code).getMetric().getName();
-		assertNotNull(daoFake.getMetricConfiguration(CONFIGURATION_NAME, metricName));
-		port.removeMetricConfiguration(CONFIGURATION_NAME, metricName);
-		assertNull(daoFake.getMetricConfiguration(CONFIGURATION_NAME, metricName));
+		port.removeMetricConfiguration(CONFIGURATION_NAME, "42");
+		verify(dao).removeMetricConfiguration(CONFIGURATION_NAME, "42");
 	}
 
 	@Test
 	public void shouldSaveConfiguration() {
-		MetricConfiguration newConfiguration = metricConfiguration("acc");
-		String metricName = newConfiguration.getMetric().getName();
-		assertNull(daoFake.getMetricConfiguration(CONFIGURATION_NAME, metricName));
-		port.saveMetricConfiguration(new MetricConfigurationXml(newConfiguration), CONFIGURATION_NAME);
-		assertDeepEquals(newConfiguration, daoFake.getMetricConfiguration(CONFIGURATION_NAME, metricName));
+		port.saveMetricConfiguration(new MetricConfigurationXml(entity), CONFIGURATION_NAME);
+		verify(dao).save(entity, CONFIGURATION_NAME);
 	}
 }
