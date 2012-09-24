c86ee21541bc6df68ca29edeb8ea1e42f363fea3
Carlos Morais <carlos88morais@gmail.com>
2012-09-24 10:52:38 -0300

    Update ConfigurationEndpointImplTest

--- KalibroTests/unit/org/kalibro/service/ConfigurationEndpointImplTest.java
+++ KalibroTests/unit/org/kalibro/service/ConfigurationEndpointImplTest.java
@@ -1,75 +1,55 @@
 package org.kalibro.service;
 
 import static org.junit.Assert.*;
-import static org.kalibro.core.model.ConfigurationFixtures.newConfiguration;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.kalibro.Configuration;
 import org.kalibro.dao.ConfigurationDao;
-import org.kalibro.dao.DaoFactory;
 import org.kalibro.service.xml.ConfigurationXml;
-import org.kalibro.tests.UnitTest;
-import org.mockito.Mockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(DaoFactory.class)
-public class ConfigurationEndpointImplTest extends UnitTest {
+@PrepareForTest(ConfigurationEndpointImpl.class)
+public class ConfigurationEndpointImplTest extends EndpointImplementorTest
+	<Configuration, ConfigurationXml, ConfigurationXml, ConfigurationDao, ConfigurationEndpointImpl> {
 
-	private ConfigurationDao dao;
-	private Configuration configuration;
-	private ConfigurationEndpointImpl endpoint;
-
-	@Before
-	public void setUp() {
-		mockDao();
-		configuration = newConfiguration();
-		endpoint = new ConfigurationEndpointImpl();
-	}
-
-	private void mockDao() {
-		dao = mock(ConfigurationDao.class);
-		mockStatic(DaoFactory.class);
-		when(DaoFactory.getConfigurationDao()).thenReturn(dao);
+	@Override
+	protected Class<Configuration> entityClass() {
+		return Configuration.class;
 	}
 
 	@Test
 	public void testSaveConfiguration() {
-		endpoint.saveConfiguration(new ConfigurationXml(configuration));
-		Mockito.verify(dao).save(configuration);
+		implementor.saveConfiguration(request);
+		verify(dao).save(entity);
 	}
 
 	@Test
 	public void testGetConfigurationNames() {
-		List<String> names = new ArrayList<String>();
+		List<String> names = mock(List.class);
 		when(dao.getConfigurationNames()).thenReturn(names);
-		assertSame(names, endpoint.getConfigurationNames());
+		assertSame(names, implementor.getConfigurationNames());
 	}
 
 	@Test
 	public void testConfirmConfiguration() {
 		when(dao.hasConfiguration("42")).thenReturn(true);
-		assertTrue(endpoint.hasConfiguration("42"));
+		assertTrue(implementor.hasConfiguration("42"));
 
 		when(dao.hasConfiguration("42")).thenReturn(false);
-		assertFalse(endpoint.hasConfiguration("42"));
+		assertFalse(implementor.hasConfiguration("42"));
 	}
 
 	@Test
 	public void testGetConfiguration() {
-		when(dao.getConfiguration("42")).thenReturn(configuration);
-		assertDeepEquals(configuration, endpoint.getConfiguration("42").convert());
+		when(dao.getConfiguration("42")).thenReturn(entity);
+		assertSame(response, implementor.getConfiguration("42"));
 	}
 
 	@Test
 	public void testRemoveConfiguration() {
-		endpoint.deleteConfiguration(42L);
-		Mockito.verify(dao).delete(42L);
+		implementor.deleteConfiguration(42L);
+		verify(dao).delete(42L);
 	}
 }
