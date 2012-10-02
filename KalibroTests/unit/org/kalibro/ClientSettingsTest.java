500567adb294187e1863f4417d621448a221a879
Carlos Morais <carlos88morais@gmail.com>
2012-10-02 00:08:10 -0300

    Refactoring DatabaseSettingsTest

--- KalibroTests/unit/org/kalibro/ClientSettingsTest.java
+++ KalibroTests/unit/org/kalibro/ClientSettingsTest.java
@@ -2,18 +2,26 @@ package org.kalibro;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.kalibro.tests.UnitTest;
 
 public class ClientSettingsTest extends UnitTest {
 
+	private ClientSettings settings;
+
+	@Before
+	public void setUp() {
+		settings = new ClientSettings();
+	}
+
 	@Test
 	public void checkConstruction() {
-		assertEquals("http://localhost:8080/KalibroService/", new ClientSettings().getServiceAddress());
+		assertEquals("http://localhost:8080/KalibroService/", settings.getServiceAddress());
 	}
 
 	@Test
 	public void shouldCommentServiceAddress() {
-		assertTrue(new ClientSettings().toString().contains(" # Address of the remote Kalibro Service\n"));
+		assertTrue(settings.toString().contains(" # Address of the remote Kalibro Service\n"));
 	}
 }
