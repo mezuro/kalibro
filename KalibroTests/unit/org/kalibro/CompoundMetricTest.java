d579a6083a52d6555b23bbd2429f57d8ed5b4793
Carlos Morais <carlos88morais@gmail.com>
2012-10-01 23:35:09 -0300

    Refactoring CompoundMetricTest

--- KalibroTests/unit/org/kalibro/CompoundMetricTest.java
+++ KalibroTests/unit/org/kalibro/CompoundMetricTest.java
@@ -2,33 +2,17 @@ package org.kalibro;
 
 import static org.junit.Assert.*;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.kalibro.tests.UnitTest;
 
 public class CompoundMetricTest extends UnitTest {
 
-	private CompoundMetric metric;
-
-	@Before
-	public void setUp() {
-		metric = new CompoundMetric();
-	}
-
 	@Test
-	public void checkInitialization() {
-		assertEquals("New metric", metric.getName());
-		assertEquals("", metric.getDescription());
-		assertEquals(Granularity.CLASS, metric.getScope());
-	}
-
-	@Test
-	public void defaultScriptShouldReturn1() {
-		assertEquals("return 1;", metric.getScript());
-	}
-
-	@Test
-	public void shouldBeCompound() {
+	public void checkConstruction() {
+		CompoundMetric metric = new CompoundMetric();
 		assertTrue(metric.isCompound());
+		assertEquals("New metric", metric.getName());
+		assertEquals(Granularity.CLASS, metric.getScope());
+		assertEquals("return 1;", metric.getScript());
 	}
 }
