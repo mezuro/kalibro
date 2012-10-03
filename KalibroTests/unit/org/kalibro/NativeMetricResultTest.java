d79027cf419aa2d56f1e785baa3370d6e17304e4
Carlos Morais <carlos88morais@gmail.com>
2012-10-03 10:54:52 -0300

    Native Result tests refactoring

--- KalibroTests/unit/org/kalibro/NativeMetricResultTest.java
+++ KalibroTests/unit/org/kalibro/NativeMetricResultTest.java
@@ -1,7 +1,6 @@
 package org.kalibro;
 
 import static org.junit.Assert.assertSame;
-import static org.kalibro.MetricFixtures.analizoMetric;
 
 import org.junit.Test;
 import org.kalibro.tests.UnitTest;
@@ -9,9 +8,9 @@ import org.kalibro.tests.UnitTest;
 public class NativeMetricResultTest extends UnitTest {
 
 	@Test
-	public void checkInitialization() {
-		NativeMetric metric = analizoMetric("dit");
-		Double value = Math.random();
+	public void checkConstruction() {
+		NativeMetric metric = mock(NativeMetric.class);
+		Double value = mock(Double.class);
 		NativeMetricResult result = new NativeMetricResult(metric, value);
 		assertSame(metric, result.getMetric());
 		assertSame(value, result.getValue());
--- KalibroTests/unit/org/kalibro/NativeModuleResultTest.java
+++ KalibroTests/unit/org/kalibro/NativeModuleResultTest.java
@@ -8,8 +8,8 @@ import org.kalibro.tests.UnitTest;
 public class NativeModuleResultTest extends UnitTest {
 
 	@Test
-	public void checkInitialization() {
-		Module module = ModuleFixtures.helloWorldApplication();
+	public void checkConstruction() {
+		Module module = mock(Module.class);
 		NativeModuleResult result = new NativeModuleResult(module);
 		assertSame(module, result.getModule());
 		assertTrue(result.getMetricResults().isEmpty());
