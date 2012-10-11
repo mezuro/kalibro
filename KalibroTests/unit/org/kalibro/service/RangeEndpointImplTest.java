37c4bf029b2d1a89f296f90b460b67e09067e8ab
Carlos Morais <carlos88morais@gmail.com>
2012-10-11 16:50:22 -0300

    RangeEndpointImpl

---
+++ KalibroTests/unit/org/kalibro/service/RangeEndpointImplTest.java
@@ -0,0 +1,43 @@
+package org.kalibro.service;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Random;
+
+import org.junit.Test;
+import org.kalibro.Range;
+import org.kalibro.dao.RangeDao;
+import org.kalibro.service.xml.RangeXmlRequest;
+import org.kalibro.service.xml.RangeXmlResponse;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+
+@PrepareForTest(RangeEndpointImpl.class)
+public class RangeEndpointImplTest extends
+	EndpointImplementorTest<Range, RangeXmlRequest, RangeXmlResponse, RangeDao, RangeEndpointImpl> {
+
+	private static final Long ID = new Random().nextLong();
+	private static final Long METRIC_CONFIGURATION_ID = new Random().nextLong();
+
+	@Override
+	protected Class<Range> entityClass() {
+		return Range.class;
+	}
+
+	@Test
+	public void shouldGetRangesOfGroup() {
+		when(dao.rangesOf(METRIC_CONFIGURATION_ID)).thenReturn(asSortedSet(entity));
+		assertDeepEquals(asList(response), implementor.rangesOf(METRIC_CONFIGURATION_ID));
+	}
+
+	@Test
+	public void shouldSave() {
+		when(dao.save(entity, METRIC_CONFIGURATION_ID)).thenReturn(ID);
+		assertEquals(ID, implementor.saveRange(request, METRIC_CONFIGURATION_ID));
+	}
+
+	@Test
+	public void shouldDeleteRange() {
+		implementor.deleteRange(ID);
+		verify(dao).delete(ID);
+	}
+}
