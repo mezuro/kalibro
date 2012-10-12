9b20babfdf122b54ca49f443054b6ca87f074428
Carlos Morais <carlos88morais@gmail.com>
2012-10-12 15:39:22 -0300

    BaseToolEndpointTest

--- KalibroTests/integration/org/kalibro/service/BaseToolEndpointTest.java
+++ KalibroTests/integration/org/kalibro/service/BaseToolEndpointTest.java
@@ -7,14 +7,22 @@ import org.kalibro.dao.BaseToolDao;
 
 public class BaseToolEndpointTest extends EndpointTest<BaseTool, BaseToolDao, BaseToolEndpoint> {
 
+	private static final String NAME = mock(String.class);
+
 	@Override
 	protected BaseTool loadFixture() {
 		return loadFixture("inexistent", BaseTool.class);
 	}
 
 	@Test
-	public void shouldGetAll() {
-		when(dao.all()).thenReturn(asSortedSet(entity));
-		assertDeepDtoList(port.allBaseTools(), entity);
+	public void shouldGetAllNames() {
+		when(dao.allNames()).thenReturn(asSortedSet(NAME));
+		assertDeepEquals(asList(NAME), port.allBaseToolNames());
+	}
+
+	@Test
+	public void shouldGetByName() {
+		when(dao.get(NAME)).thenReturn(entity);
+		assertDeepDtoEquals(entity, port.getBaseTool(NAME));
 	}
 }
