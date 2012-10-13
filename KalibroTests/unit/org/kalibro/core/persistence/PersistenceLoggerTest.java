132ab52346c7077dd8efdc409c2e06c7619ce20a
Carlos Morais <carlos88morais@gmail.com>
2012-10-12 23:35:37 -0300

    PersistenceLogger

--- KalibroTests/unit/org/kalibro/core/persistence/PersistenceLoggerTest.java
+++ KalibroTests/unit/org/kalibro/core/persistence/PersistenceLoggerTest.java
@@ -1,6 +1,7 @@
 package org.kalibro.core.persistence;
 
 import static org.junit.Assert.*;
+import static org.kalibro.core.Environment.logsDirectory;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -8,7 +9,6 @@ import java.io.FileWriter;
 import org.eclipse.persistence.logging.DefaultSessionLog;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.kalibro.core.Environment;
 import org.kalibro.tests.UnitTest;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -24,7 +24,7 @@ public class PersistenceLoggerTest extends UnitTest {
 
 	@Test
 	public void shouldWriteOnLogsDirectory() throws Exception {
-		File logFile = new File(Environment.logsDirectory(), "JPA.log");
+		File logFile = new File(logsDirectory(), "JPA.log");
 		FileWriter writer = mock(FileWriter.class);
 		whenNew(FileWriter.class).withArguments(logFile).thenReturn(writer);
 
