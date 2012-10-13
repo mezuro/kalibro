60984e4eb3c55eff12ced8fe25fb755c35db4413
Carlos Morais <carlos88morais@gmail.com>
2012-10-12 21:53:01 -0300

    LocalDirectoryTest

--- KalibroTests/integration/org/kalibro/core/loaders/LocalDirectoryTest.java
+++ KalibroTests/integration/org/kalibro/core/loaders/LocalDirectoryTest.java
@@ -1,11 +1,9 @@
 package org.kalibro.core.loaders;
 
-import org.kalibro.RepositoryType;
-
 public class LocalDirectoryTest extends RepositoryIntegrationTest {
 
 	@Override
-	protected RepositoryType getRepositoryType() {
-		return RepositoryType.LOCAL_DIRECTORY;
+	protected String address() {
+		return repositoriesDirectory().getAbsolutePath() + "/HelloWorldDirectory/";
 	}
 }
