ecacfb2b3cebe37ebe5db122b3076c21de6a4f43
Carlos Morais <carlos88morais@gmail.com>
2012-10-22 12:30:52 -0200

    AuditAdapter documented.

--- KalibroCore/src/org/checkstyle/AuditAdapter.java
+++ KalibroCore/src/org/checkstyle/AuditAdapter.java
@@ -3,7 +3,14 @@ package org.checkstyle;
 import com.puppycrawl.tools.checkstyle.api.AuditEvent;
 import com.puppycrawl.tools.checkstyle.api.AuditListener;
 
-public abstract class AuditAdapter implements AuditListener {
+/**
+ * An abstract adapter class for receiving {@link AuditEvent}s. The methods in this class are empty. This class exists
+ * as convenience for creating {@link AuditListener} implementations.
+ * 
+ * @author Carlos Morais
+ * @author Eduardo Morais
+ */
+abstract class AuditAdapter implements AuditListener {
 
 	@Override
 	public void auditStarted(AuditEvent event) {
