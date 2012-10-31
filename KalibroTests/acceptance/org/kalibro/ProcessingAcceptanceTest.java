dc6145994f05c95333adda76b040385a7b46114f
Carlos Morais <carlos88morais@gmail.com>
2012-10-31 11:38:41 -0200

    ProcessingAcceptanceTest for normal results

--- KalibroTests/acceptance/org/kalibro/ProcessingAcceptanceTest.java
+++ KalibroTests/acceptance/org/kalibro/ProcessingAcceptanceTest.java
@@ -1,6 +1,8 @@
 package org.kalibro;
 
 import static org.junit.Assert.*;
+import static org.kalibro.Granularity.*;
+import static org.kalibro.ProcessState.*;
 
 import org.junit.Before;
 import org.junit.experimental.theories.Theory;
@@ -9,8 +11,12 @@ import org.kalibro.tests.AcceptanceTest;
 
 public class ProcessingAcceptanceTest extends AcceptanceTest {
 
+	private static final long SLEEP = 10000;
+
 	private Repository repository;
 	private Configuration configuration;
+
+	private Processing processing;
 
 	@Before
 	public void setUp() {
@@ -31,12 +37,44 @@ public class ProcessingAcceptanceTest extends AcceptanceTest {
 		resetDatabase(databaseType);
 		assertFalse(Processing.hasProcessing(repository));
 
+		long processingTime = System.currentTimeMillis();
 		repository.process();
 		assertTrue(Processing.hasProcessing(repository));
 		assertFalse(Processing.hasReadyProcessing(repository));
 		assertTrue(Processing.lastProcessingState(repository).isTemporary());
-		Thread.sleep(10000);
+		Thread.sleep(SLEEP);
 
-		assertEquals(ProcessState.READY, Processing.lastProcessingState(repository));
+		processing = Processing.lastProcessing(repository);
+		assertEquals(processingTime, processing.getDate().getTime(), 500);
+		verifyNormalResults();
+	}
+
+	private void verifyNormalResults() {
+		assertEquals(READY, processing.getState());
+		verifyStateTime(LOADING);
+		verifyStateTime(COLLECTING);
+		verifyStateTime(ANALYZING);
+
+		Metric totalCof = metric("total_cof", false);
+		ModuleResult root = processing.getResultsRoot();
+		assertDeepEquals(new Module(SOFTWARE, "null"), root.getModule());
+		assertDoubleEquals(1.0, root.getResultFor(totalCof).getValue());
+
+		Metric sc = metric("sc", true);
+		Metric lcom4 = metric("lcom4", false);
+		ModuleResult child = root.getChildren().first();
+		assertDeepEquals(new Module(CLASS, "HelloWorld"), child.getModule());
+		assertDoubleEquals(0.0, child.getResultFor(sc).getValue());
+		assertDoubleEquals(1.0, child.getResultFor(lcom4).getValue());
+		assertDoubleEquals(10.0, child.getResultFor(lcom4).getGrade());
+	}
+
+	private void verifyStateTime(ProcessState state) {
+		Long stateTime = processing.getStateTime(state);
+		assertTrue("Time for " + state + ": " + stateTime, 0 < stateTime && stateTime < SLEEP);
+	}
+
+	private Metric metric(String name, boolean compound) {
+		return loadFixture(name, compound ? CompoundMetric.class : NativeMetric.class);
 	}
 }
