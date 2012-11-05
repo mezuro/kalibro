9adb055429faac1b211bfcd6bf6a6e9b9c56a7fe
Carlos Morais <carlos88morais@gmail.com>
2012-11-05 14:52:33 -0200

    Refactor FieldSizeTest

--- KalibroDesktopTests/unit/org/kalibro/desktop/swingextension/field/FieldSizeTest.java
+++ KalibroDesktopTests/unit/org/kalibro/desktop/swingextension/field/FieldSizeTest.java
@@ -11,40 +11,39 @@ import javax.swing.JComponent;
 import org.junit.Before;
 import org.junit.Test;
 import org.kalibro.tests.UnitTest;
-import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
 
 public class FieldSizeTest extends UnitTest {
 
-	private static final Random RANDOM = new Random(System.currentTimeMillis());
-	private static final int WIDTH = RANDOM.nextInt();
-	private static final int HEIGHT = RANDOM.nextInt();
-	private static final int FONT_SIZE = RANDOM.nextInt();
-
+	private Font font;
+	private Dimension componentSize;
 	private JComponent component;
+
 	private FieldSize fieldSize;
 
 	@Before
 	public void setUp() {
-		component = PowerMockito.mock(JComponent.class);
-		PowerMockito.when(component.getPreferredSize()).thenReturn(new Dimension(WIDTH, HEIGHT));
-		PowerMockito.when(component.getFont()).thenReturn(new Font("Arial", Font.BOLD, FONT_SIZE));
+		Random random = new Random(System.currentTimeMillis());
+		font = new Font("Arial", Font.BOLD, random.nextInt());
+		componentSize = new Dimension(random.nextInt(), random.nextInt());
+		component = mock(JComponent.class);
+		when(component.getFont()).thenReturn(font);
+		when(component.getPreferredSize()).thenReturn(componentSize);
 		fieldSize = new FieldSize(component);
 	}
 
 	@Test
-	public void shouldGetWidthFromComponent() {
-		assertEquals(WIDTH, fieldSize.width);
+	public void shouldGetWidthFromComponentSize() {
+		assertEquals(componentSize.width, fieldSize.width);
 	}
 
 	@Test
 	public void shouldGetHeightFromFontSize() {
-		assertEquals(2 * FONT_SIZE, fieldSize.height);
+		assertEquals(2 * font.getSize(), fieldSize.height);
 	}
 
 	@Test
 	public void shouldSetMinimumAndPreferredSize() {
-		Mockito.verify(component).setMinimumSize(fieldSize);
-		Mockito.verify(component).setPreferredSize(fieldSize);
+		verify(component).setMinimumSize(fieldSize);
+		verify(component).setPreferredSize(fieldSize);
 	}
 }
