3fedf83cf596c3b50b9cab2939e28ff4fd3f2481
Carlos Morais <carlos88morais@gmail.com>
2012-11-05 16:15:05 -0200

    FileChooser

--- KalibroDesktopTests/unit/org/kalibro/desktop/swingextension/dialog/FileChooserTest.java
+++ KalibroDesktopTests/unit/org/kalibro/desktop/swingextension/dialog/FileChooserTest.java
@@ -2,6 +2,7 @@ package org.kalibro.desktop.swingextension.dialog;
 
 import static javax.swing.JFileChooser.*;
 import static org.junit.Assert.*;
+import static org.kalibro.core.Environment.dotKalibro;
 
 import java.awt.Component;
 import java.io.File;
@@ -12,11 +13,8 @@ import javax.swing.filechooser.FileFilter;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.kalibro.core.Environment;
 import org.kalibro.tests.UnitTest;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
@@ -31,22 +29,22 @@ public class FileChooserTest extends UnitTest {
 
 	@Before
 	public void setUp() throws Exception {
-		parent = PowerMockito.mock(Component.class);
-		nativeChooser = PowerMockito.mock(JFileChooser.class);
-		PowerMockito.whenNew(JFileChooser.class).withNoArguments().thenReturn(nativeChooser);
+		parent = mock(Component.class);
+		nativeChooser = mock(JFileChooser.class);
+		whenNew(JFileChooser.class).withNoArguments().thenReturn(nativeChooser);
 		fileChooser = new FileChooser(parent);
 	}
 
 	@Test
 	public void shouldNotAllowMultipleSelection() {
-		Mockito.verify(nativeChooser).setMultiSelectionEnabled(false);
+		verify(nativeChooser).setMultiSelectionEnabled(false);
 	}
 
 	@Test
 	public void shouldShowOnlyDirectoriesWhenChoosingDirectory() {
 		fileChooser.chooseDirectoryToOpen();
-		Mockito.verify(nativeChooser).setFileSelectionMode(DIRECTORIES_ONLY);
-		Mockito.verify(nativeChooser).setAcceptAllFileFilterUsed(false);
+		verify(nativeChooser).setFileSelectionMode(DIRECTORIES_ONLY);
+		verify(nativeChooser).setAcceptAllFileFilterUsed(false);
 	}
 
 	@Test
@@ -55,61 +53,61 @@ public class FileChooserTest extends UnitTest {
 		FileFilter filter = captureFileFilter();
 
 		assertEquals("Directories only", filter.getDescription());
-		assertTrue(filter.accept(Environment.dotKalibro()));
-		assertFalse(filter.accept(new File(Environment.dotKalibro(), "HelloWorld.c")));
+		assertTrue(filter.accept(dotKalibro()));
+		assertFalse(filter.accept(new File(dotKalibro(), "HelloWorld.c")));
 	}
 
 	private FileFilter captureFileFilter() {
 		ArgumentCaptor<FileFilter> captor = ArgumentCaptor.forClass(FileFilter.class);
-		Mockito.verify(nativeChooser).setFileFilter(captor.capture());
+		verify(nativeChooser).setFileFilter(captor.capture());
 		return captor.getValue();
 	}
 
 	@Test
 	public void shouldShowOnlyFilesWhenChoosingFile() {
 		fileChooser.chooseFileToSave("");
-		Mockito.verify(nativeChooser).setFileSelectionMode(FILES_ONLY);
+		verify(nativeChooser).setFileSelectionMode(FILES_ONLY);
 	}
 
 	@Test
 	public void shouldReturnTrueWhenSelected() {
-		PowerMockito.when(nativeChooser.showOpenDialog(parent)).thenReturn(APPROVE_OPTION);
-		assertTrue(fileChooser.chooseDirectoryToOpen());
+		when(nativeChooser.showOpenDialog(parent)).thenReturn(APPROVE_OPTION);
+		when(nativeChooser.showSaveDialog(parent)).thenReturn(APPROVE_OPTION);
 
-		PowerMockito.when(nativeChooser.showSaveDialog(parent)).thenReturn(APPROVE_OPTION);
+		assertTrue(fileChooser.chooseDirectoryToOpen());
 		assertTrue(fileChooser.chooseFileToSave(""));
 	}
 
 	@Test
 	public void shouldReturnFalseWhenCancelled() {
-		PowerMockito.when(nativeChooser.showOpenDialog(parent)).thenReturn(CANCEL_OPTION);
-		assertFalse(fileChooser.chooseDirectoryToOpen());
+		when(nativeChooser.showOpenDialog(parent)).thenReturn(CANCEL_OPTION);
+		when(nativeChooser.showSaveDialog(parent)).thenReturn(CANCEL_OPTION);
 
-		PowerMockito.when(nativeChooser.showSaveDialog(parent)).thenReturn(CANCEL_OPTION);
+		assertFalse(fileChooser.chooseDirectoryToOpen());
 		assertFalse(fileChooser.chooseFileToSave(""));
 	}
 
 	@Test
 	public void shouldReturnFalseWhenClosed() {
-		PowerMockito.when(nativeChooser.showOpenDialog(parent)).thenReturn(ERROR_OPTION);
-		assertFalse(fileChooser.chooseDirectoryToOpen());
+		when(nativeChooser.showOpenDialog(parent)).thenReturn(ERROR_OPTION);
+		when(nativeChooser.showSaveDialog(parent)).thenReturn(ERROR_OPTION);
 
-		PowerMockito.when(nativeChooser.showSaveDialog(parent)).thenReturn(ERROR_OPTION);
+		assertFalse(fileChooser.chooseDirectoryToOpen());
 		assertFalse(fileChooser.chooseFileToSave(""));
 	}
 
 	@Test
 	public void shouldSelectSuggestionInsideCurrentDirectory() {
-		PowerMockito.when(nativeChooser.getCurrentDirectory()).thenReturn(Environment.dotKalibro());
+		when(nativeChooser.getCurrentDirectory()).thenReturn(dotKalibro());
 
 		String suggestion = "MyFile.txt";
 		fileChooser.chooseFileToSave(suggestion);
-		Mockito.verify(nativeChooser).setSelectedFile(new File(Environment.dotKalibro(), suggestion));
+		verify(nativeChooser).setSelectedFile(new File(dotKalibro(), suggestion));
 	}
 
 	@Test
 	public void checkChoosenFile() {
-		PowerMockito.when(nativeChooser.getSelectedFile()).thenReturn(Environment.dotKalibro());
-		assertEquals(Environment.dotKalibro(), fileChooser.getChosenFile());
+		when(nativeChooser.getSelectedFile()).thenReturn(dotKalibro());
+		assertEquals(dotKalibro(), fileChooser.getChosenFile());
 	}
 }
