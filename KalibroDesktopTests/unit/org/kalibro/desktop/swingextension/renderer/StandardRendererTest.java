2d7deb9942f548fea586cc26e8d7a8322593883f
Carlos Morais <carlos88morais@gmail.com>
2012-11-06 17:12:42 -0200

    StandardRenderer

--- KalibroDesktopTests/unit/org/kalibro/desktop/swingextension/renderer/StandardRendererTest.java
+++ KalibroDesktopTests/unit/org/kalibro/desktop/swingextension/renderer/StandardRendererTest.java
@@ -3,21 +3,30 @@ package org.kalibro.desktop.swingextension.renderer;
 import org.junit.Before;
 import org.junit.Test;
 import org.kalibro.tests.UnitTest;
-import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
 
 public class StandardRendererTest extends UnitTest {
+
+	private Object value, context;
 
 	private StandardRenderer renderer;
 
 	@Before
-	public void setUp() {
-		renderer = PowerMockito.spy(new NullRenderer());
+	public void setUp() throws Exception {
+		value = mock(Object.class);
+		context = mock(Object.class);
+		renderer = mockAbstract(StandardRenderer.class);
+		doReturn(null).when(renderer).render(value);
+
+		renderer.render(value, context);
 	}
 
 	@Test
-	public void shouldRenderIgnoringContext() {
-		renderer.render("42", "24");
-		Mockito.verify(renderer).render("42");
+	public void shouldRenderValue() {
+		verify(renderer).render(value);
+	}
+
+	@Test
+	public void shouldIgnoreContext() {
+		verifyZeroInteractions(context);
 	}
 }
