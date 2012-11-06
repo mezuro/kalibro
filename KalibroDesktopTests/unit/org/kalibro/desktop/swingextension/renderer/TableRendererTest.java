7aee8255a9277d270774822ab3237d04e709c1fd
Carlos Morais <carlos88morais@gmail.com>
2012-11-06 17:12:28 -0200

    TableRenderer

--- KalibroDesktopTests/unit/org/kalibro/desktop/swingextension/renderer/TableRendererTest.java
+++ KalibroDesktopTests/unit/org/kalibro/desktop/swingextension/renderer/TableRendererTest.java
@@ -12,59 +12,53 @@ import org.junit.Before;
 import org.junit.Test;
 import org.kalibro.desktop.swingextension.list.TableModel;
 import org.kalibro.tests.UnitTest;
-import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
 
 public class TableRendererTest extends UnitTest {
 
-	private boolean isSelected;
 	private Component component, renderedComponent;
+	private Object value, context;
+	private boolean isSelected;
 
 	private TableRenderer renderer;
 
 	@Before
-	public void setUp() {
-		component = PowerMockito.mock(Component.class);
-		renderer = PowerMockito.spy(new MyRenderer());
+	public void setUp() throws Exception {
+		value = mock(Object.class);
+		context = mock(Object.class);
+		component = mock(Component.class);
+		renderer = mockAbstract(TableRenderer.class);
+		doReturn(component).when(renderer).render(any(), any());
 		render();
 	}
 
 	private void render() {
-		Random random = new Random(System.currentTimeMillis());
+		Random random = new Random();
 		isSelected = random.nextBoolean();
 		boolean hasFocus = random.nextBoolean();
-		int column = random.nextInt();
+		int column = random.nextInt(), view = random.nextInt(), row = random.nextInt();
 		Color background = new Color(random.nextInt());
-		PowerMockito.when(component.getBackground()).thenReturn(background);
+		when(component.getBackground()).thenReturn(background);
 
-		JTable table = PowerMockito.mock(JTable.class);
-		TableModel<String> model = PowerMockito.mock(TableModel.class);
-		PowerMockito.when(table.getModel()).thenReturn(model);
-		PowerMockito.when(table.convertRowIndexToModel(24)).thenReturn(42);
-		PowerMockito.when(model.getObjectAt(42)).thenReturn("context");
-		renderedComponent = renderer.getTableCellRendererComponent(table, "value", isSelected, hasFocus, 24, column);
+		JTable table = mock(JTable.class);
+		TableModel<Object> model = mock(TableModel.class);
+		when(table.getModel()).thenReturn(model);
+		when(table.convertRowIndexToModel(view)).thenReturn(row);
+		when(model.getObjectAt(row)).thenReturn(context);
+		renderedComponent = renderer.getTableCellRendererComponent(table, value, isSelected, hasFocus, view, column);
 	}
 
 	@Test
 	public void shouldRenderValueWithContext() {
-		Mockito.verify(renderer).render("value", "context");
+		verify(renderer).render(value, context);
 	}
 
 	@Test
-	public void shouldChangeBackgroundIfSelected() {
-		Mockito.verify(renderer).setSelectionBackground(component, isSelected);
+	public void shouldSetSelectionBackground() {
+		verify(renderer).setSelectionBackground(component, isSelected);
 	}
 
 	@Test
 	public void shouldReturnRenderedComponent() {
 		assertSame(component, renderedComponent);
-	}
-
-	private class MyRenderer extends TableRenderer {
-
-		@Override
-		protected Component render(Object value, Object context) {
-			return component;
-		}
 	}
 }
