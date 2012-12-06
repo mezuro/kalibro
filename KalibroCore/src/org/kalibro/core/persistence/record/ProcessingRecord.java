e7e3cb3a5a407633be660031ed65a27135e3d17a
Carlos Morais <carlos88morais@gmail.com>
2012-12-06 16:47:57 -0200

    Fix remove @CascadeOnDelete without mappedBy

--- KalibroCore/src/org/kalibro/core/persistence/record/ProcessingRecord.java
+++ KalibroCore/src/org/kalibro/core/persistence/record/ProcessingRecord.java
@@ -44,7 +44,6 @@ public class ProcessingRecord extends ProcessingDto {
 	private Collection<ProcessTimeRecord> processTimes;
 
 	@OneToOne
-	@CascadeOnDelete
 	@JoinColumn(name = "\"results_root\"", referencedColumnName = "\"id\"")
 	private ModuleResultRecord resultsRoot;
 
