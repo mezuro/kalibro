ce671383b6b2053c894629a24afb846894721e28
Carlos Morais <carlos88morais@gmail.com>
2012-12-18 16:20:58 -0200

    Fix ImportConfigurationTest

--- KalibroTests/unit/org/kalibro/ImportConfigurationTest.java
+++ KalibroTests/unit/org/kalibro/ImportConfigurationTest.java
@@ -4,9 +4,7 @@ import java.io.File;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.kalibro.client.ClientDaoFactory;
 import org.kalibro.core.concurrent.VoidTask;
-import org.kalibro.dao.ConfigurationDao;
 import org.kalibro.tests.UtilityClassTest;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -16,38 +14,28 @@ import org.powermock.modules.junit4.PowerMockRunner;
 public class ImportConfigurationTest extends UtilityClassTest {
 
 	private static final String FILE_PATH = "ImportConfigurationTest file path";
-	private static final String SERVICE_ADDRESS = "ImportConfigurationTest service address";
 
 	@Test
-	public void shouldThrowExceptionForInvalidNumberOfArguments() {
-		shouldThrowExceptionFor();
-		shouldThrowExceptionFor(".");
-	}
-
-	private void shouldThrowExceptionFor(final String... arguments) {
+	public void shouldThrowExceptionForNoArguments() {
 		assertThat(new VoidTask() {
 
 			@Override
 			protected void perform() throws Throwable {
-				ImportConfiguration.main(arguments);
+				ImportConfiguration.main(new String[0]);
 			}
-		}).doThrow(IllegalArgumentException.class).withMessage("Expected 2 arguments: file path and service address.");
+		}).doThrow(IllegalArgumentException.class).withMessage("Expected configuration file path as argument.");
 	}
 
 	@Test
 	public void shouldSaveConfigurationOnService() throws Exception {
 		File file = mock(File.class);
 		Configuration configuration = mock(Configuration.class);
-		ClientDaoFactory daoFactory = mock(ClientDaoFactory.class);
-		ConfigurationDao dao = mock(ConfigurationDao.class);
 
 		mockStatic(Configuration.class);
 		whenNew(File.class).withArguments(FILE_PATH).thenReturn(file);
-		whenNew(ClientDaoFactory.class).withArguments(SERVICE_ADDRESS).thenReturn(daoFactory);
 		when(Configuration.importFrom(file)).thenReturn(configuration);
-		when(daoFactory.createConfigurationDao()).thenReturn(dao);
 
-		ImportConfiguration.main(array(FILE_PATH, SERVICE_ADDRESS));
-		verify(dao).save(configuration);
+		ImportConfiguration.main(array(FILE_PATH));
+		verify(configuration).save();
 	}
 }
