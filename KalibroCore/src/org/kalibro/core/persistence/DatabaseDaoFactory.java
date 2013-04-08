e141e7475dcbc91ec47c6eaee8162b8468ff32de
Daniel Alves + Diego Araújo <diegoamc90@gmail.com>
2013-04-08 17:40:26 -0300

    Fixed a database dao factory method.

--- KalibroCore/src/org/kalibro/core/persistence/DatabaseDaoFactory.java
+++ KalibroCore/src/org/kalibro/core/persistence/DatabaseDaoFactory.java
@@ -24,7 +24,7 @@ public class DatabaseDaoFactory extends DaoFactory {
 	private static EntityManagerFactory entityManagerFactory;
 
 	static RecordManager createRecordManager() {
-		if (entityManagerFactory == null || !entityManagerFactory.isOpen())
+		if (entityManagerFactory == null || ! entityManagerFactory.isOpen())
 			updateSettings(currentSettings);
 		return new RecordManager(entityManagerFactory.createEntityManager());
 	}
@@ -49,7 +49,7 @@ public class DatabaseDaoFactory extends DaoFactory {
 	}
 
 	public DatabaseDaoFactory(DatabaseSettings settings) {
-		if (!settings.deepEquals(currentSettings))
+		if (! settings.deepEquals(currentSettings))
 			updateSettings(settings);
 	}
 
@@ -110,6 +110,6 @@ public class DatabaseDaoFactory extends DaoFactory {
 
 	@Override
 	protected ProcessingNotificationDao createProcessingNotificationDao() {
-		return ProcessingNotificationDatabaseDao();
+		return new ProcessingNotificationDatabaseDao();
 	}
 }
