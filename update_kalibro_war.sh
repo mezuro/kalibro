01f935fc4f59b66166bba6365aaf11b61cd05d1f
Daniel Alves + Diego Araújo + Guilherme Rojas <danpaulalves@gmail.com>
2013-04-23 17:38:01 -0300

    Script now refreshes database for postgres.

--- update_kalibro_war.sh
+++ update_kalibro_war.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-validate() {
-	unset ANSWER
-	while ! [ "$ANSWER" == "y" ]
-	do	
-		echo -e "\t > Are you done? (y)"
-		read ANSWER
-	done
-}
-
 TOMCAT_HOME="/var/lib/tomcat6/webapps"
 
-echo "* Exporting Kalibro .war file."
-#ant -f build.xml
 sudo service tomcat6 stop
+
+#echo " * Exporting new Kalibro Service."
+#ant -f build.xml
+
+echo " * Updating Kalibro Service."
 sudo rm -rf $TOMCAT_HOME/KalibroService
+sudo chown tomcat6:tomcat6 KalibroService.war
 sudo mv KalibroService.war $TOMCAT_HOME
 
-echo "* Please, drop your kalibro database now."
-validate
-echo "* Please, create your kalibro database again."
-validate
+echo " * Erasing old kalibro database entries."
+echo -en "\t > Please, type your kalibro database password: "
+stty -echo
+read
+stty echo
+echo
+export PGPASSWORD=$REPLY
+psql -q -U postgres -c "drop database kalibro"
+psql -q -U postgres -c "drop database kalibro_test"
+psql -q -U postgres -c "create database kalibro with owner=kalibro"
+psql -q -U postgres -c "create database kalibro_test with owner=kalibro"
 sudo service tomcat6 start
