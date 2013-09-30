0a7dcb5f700d85b5332176b389fbdf9aa3e0d864
Carlos Morais <carlos88morais@gmail.com>
2013-09-30 20:34:27 -0300

    CollectingTask using ProcessContext

--- KalibroTests/unit/org/kalibro/core/processing/CollectingTaskTest.java
+++ KalibroTests/unit/org/kalibro/core/processing/CollectingTaskTest.java
@@ -3,64 +3,53 @@ package org.kalibro.core.processing;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.kalibro.*;
 import org.kalibro.core.concurrent.Producer;
 import org.kalibro.core.concurrent.Writer;
 import org.kalibro.tests.UnitTest;
-import org.powermock.core.classloader.annotations.PrepareOnlyThisForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
-@RunWith(PowerMockRunner.class)
-@PrepareOnlyThisForTest(CollectingTask.class)
 public class CollectingTaskTest extends UnitTest {
 
+	private static final int BASE_TOOL_COUNT = 1 + new Random().nextInt(5);
+
 	private File codeDirectory;
-	private Configuration configuration;
 	private Writer<NativeModuleResult> resultWriter;
-
-	private CollectingTask collectingTask;
-
-	@Before
-	public void setUp() {
-		collectingTask = spy(new CollectingTask());
-		mockCodeDirectory();
-		mockConfiguration();
-		mockResultWriter();
-	}
-
-	private void mockCodeDirectory() {
-		codeDirectory = mock(File.class);
-		doReturn(codeDirectory).when(collectingTask).codeDirectory();
-	}
-
-	private void mockConfiguration() {
-		configuration = mock(Configuration.class);
-		Repository repository = mock(Repository.class);
-		doReturn(repository).when(collectingTask).repository();
-		when(repository.getConfiguration()).thenReturn(configuration);
-	}
-
-	private void mockResultWriter() {
-		resultWriter = mock(Writer.class);
-		Producer<NativeModuleResult> resultProducer = mock(Producer.class);
-		doReturn(resultProducer).when(collectingTask).resultProducer();
-		when(resultProducer.createWriter()).thenReturn(resultWriter);
-	}
+	private Producer<NativeModuleResult> resultProducer;
+	private Map<BaseTool, Set<NativeMetric>> wantedMetrics;
 
 	@Test
 	public void shouldCollectMetricsFromBaseTools() throws Exception {
-		BaseTool baseTool = mock(BaseTool.class);
-		Set<NativeMetric> wantedMetrics = mock(Set.class);
-		Map<BaseTool, Set<NativeMetric>> wantedMetricsMap = new HashMap<BaseTool, Set<NativeMetric>>();
-		wantedMetricsMap.put(baseTool, wantedMetrics);
-		when(configuration.getNativeMetrics()).thenReturn(wantedMetricsMap);
+		codeDirectory = mock(File.class);
+		resultWriter = mock(Writer.class);
+		resultProducer = mock(Producer.class);
+		mockWantedMetrics();
+		new CollectingTask(mockContext()).perform();
 
-		collectingTask.perform();
-		verify(baseTool).collectMetrics(codeDirectory, wantedMetrics, resultWriter);
+		for (BaseTool baseTool : wantedMetrics.keySet())
+			verify(baseTool).collectMetrics(codeDirectory, wantedMetrics.get(baseTool), resultWriter);
+		verify(resultProducer, times(BASE_TOOL_COUNT)).createWriter();
+	}
+
+	private void mockWantedMetrics() {
+		wantedMetrics = new HashMap<BaseTool, Set<NativeMetric>>();
+		for (int i = 0; i < BASE_TOOL_COUNT; i++)
+			wantedMetrics.put(mock(BaseTool.class), mock(Set.class));
+	}
+
+	private ProcessContext mockContext() {
+		Repository repository = mock(Repository.class);
+		Configuration configuration = mock(Configuration.class);
+		ProcessContext processContext = mock(ProcessContext.class);
+		when(processContext.codeDirectory()).thenReturn(codeDirectory);
+		when(processContext.repository()).thenReturn(repository);
+		when(repository.getConfiguration()).thenReturn(configuration);
+		when(configuration.getNativeMetrics()).thenReturn(wantedMetrics);
+		when(processContext.resultProducer()).thenReturn(resultProducer);
+		when(resultProducer.createWriter()).thenReturn(resultWriter);
+		return processContext;
 	}
 }
